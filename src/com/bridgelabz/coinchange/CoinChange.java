5231335b0e0f13a84471ac6eb2ad64a543672b47
a-faisalkhan <kfaisalit@gmail.com>
2022-02-27 18:02:57 +0530

    test cases check up to 10 in flipcoin program

--- src/com/bridgelabz/coinchange/CoinChange.java
+++ src/com/bridgelabz/coinchange/CoinChange.java
@@ -1,24 +1,44 @@
 package com.bridgelabz.coinchange;
 
+import java.util.Arrays;
+
 public class CoinChange {
 	
-	public int minCoin(int amount, int [] coins) {
+	public static void main(String[] args) {
+
+		int[] currency = { 1, 2, 5, 10, 20, 50, 100, 500, 1000 };
+		//int amount = 219;
+	    int amount = 550; 	
+
+		int[] ds = new int[amount + 1];
+
+		Arrays.fill(ds, -1);
+
+		int minChange = minCoin(amount, currency, ds);
+		System.out.println("Min change required : " + minChange);
+	}
+
+	public static int minCoin(int amount, int[] coins, int[] ds) {
 		if (amount == 0) {
-			return 0; 
-		
+			return 0;
 		} else {
-			//int subAns = 0;
-			int ans = Integer.MAX_VALUE; 
-			int subAns = 0; 
+			int ans = Integer.MAX_VALUE;
+
 			for (int i = 0; i < coins.length; i++) {
-				if (amount - coins [i] >= 0 && subAns < ans) {
-					subAns = minCoin(amount - coins[i], coins); //minCoins (1, {1,5,7}); 
-					ans = subAns + 1; 
+				int subAns = 0;
+				if (amount - coins[i] >= 0) {
+					if (ds[amount - coins[i]] != -1) {
+						subAns = ds[amount - coins[i]];
+					} else {
+						subAns = minCoin(amount - coins[i], coins, ds); // minCoin(1,{1,5,7});
+					}
+					if (ans > subAns) {
+						ans = subAns + 1;
+					}
 				}
 			}
-			//int ans = subAns + 1; 
-			
-			return ans; 
+			ds[amount] = ans;
+			return ans;
 		}
 		
 	}
--- src/com/bridgelabz/coinchange/CoinChangeTest.java
+++ src/com/bridgelabz/coinchange/CoinChangeTest.java
@@ -1,5 +1,39 @@
 package com.bridgelabz.coinchange;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
 public class CoinChangeTest {
 
+	/*
+	 * @Test public void minCoin0() { assertEquals(0, CoinChange.minCoin(0, new
+	 * int[] { 1, 5, 7 })); }
+	 * 
+	 * @Test public void minCoin1() { assertEquals(1, CoinChange.minCoin(1, new
+	 * int[] { 1, 5, 7 })); }
+	 */
+
+	/*
+	 * @Test public void minCoin2() { assertEquals(2, CoinChange.minCoin(2, new
+	 * int[] { 1, 5, 7 })); }
+	 */
+
+	/*
+	 * @Test public void minCoin4() { assertEquals(4, CoinChange.minCoin(4, new
+	 * int[] { 1, 5, 7 })); }
+	 */
+
+	@Test
+	public void minCoin5() {
+//		int amount = 51;
+//		int expectedOp = 9;
+		int amount = 6;
+		int expectedOp = 2;
+		int ds[] = new int[amount + 1];
+		Arrays.fill(ds, -1);
+		assertEquals(expectedOp, CoinChange.minCoin(amount, new int[] { 1, 5, 7 }, ds));
+	}
 }
